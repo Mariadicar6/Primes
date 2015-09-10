6d3088435840f09b3b2c746d29e20651903708c1
Maria <mariadicar.28@gmail.com>
2015-09-10 01:12:53 -0500

    Arreglando oscurecimiento de fotos

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	$("#banner").css({"height":$(window).height() + "px"});
+
+	var flag = false;
+	var scroll;
+
+	$(window).scroll(function(){
+		scroll = $(window).scrollTop();
+
+		if(scroll > 200){
+			if(!flag){
+				$("header").css({"background-color": "#3C3C3C"});
+				flag = true;
+			}
+		}else{
+			if(flag){
+				$("header").css({"background-color": "transparent"});
+				flag = false;
+			}
+		}
+
+
+	});
+
+});
